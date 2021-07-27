741add7c87edf75aa32365c96821970556625432
Faitholo <ikhidero.faithy@gmail.com>
2021-07-27 16:25:14 +0100

    Update get_func.c

--- get_func.c
+++ get_func.c
@@ -3,7 +3,7 @@
 * get_func - search and return the correct function
 * @s: array to check
 * Return: return a function (if written correctly)
-*/
+**/
 int (*get_func(char s))(va_list)
 {
 	op_t ops[] = {
@@ -17,7 +17,7 @@ int (*get_func(char s))(va_list)
 
 	int i;
 
-	for (i = 0; ops[i].c != NULL; i++);
+	for (i = 0; ops[i].c != NULL; i++)
 	{
 		if (*ops[i].c == s)
 			return (ops[i].f);
